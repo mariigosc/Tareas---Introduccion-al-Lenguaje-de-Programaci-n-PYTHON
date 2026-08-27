b73821f6b3f77113c89a695d2bcb2a6b081062b4
Constanza Mariana González Sierra <cmarygonsierra@gmail.com>
2026-08-26 21:39:28 -0600

    Add files via upload
    Tarea clase 24 agosto 2026

---
+++ Tarea Sierpinski triangle n = 2.py
@@ -0,0 +1,36 @@
+import turtle
+
+# Configuración básica para n = 2
+
+turtle.speed(4)                 # Velocidad moderada para observar el trazo
+                                # speed rendimiento del código al ejecutarse y a la rapidez con la que se pueden desarrollar programas con este lenguaje
+
+linea = 'F-G-G'
+n = 2
+angulo = 120
+longitud_paso = 40              # Trazo largo porque hay pocos pasos
+
+
+
+# string
+for iteracion in range(n):
+    nueva_linea = ""
+    for caracter in linea:
+        if caracter == 'F':
+            nueva_linea = nueva_linea + "F-G+F+G-F"
+        elif caracter == 'G':
+            nueva_linea = nueva_linea + "GG"
+        else:
+            nueva_linea = nueva_linea + caracter
+    linea = nueva_linea
+
+# Dibujo del fractal
+for instruccion in linea:
+    if instruccion == 'F' or instruccion == 'G':
+        turtle.forward(longitud_paso)
+    elif instruccion == '+':
+        turtle.left(angulo)
+    elif instruccion == '-':
+        turtle.right(angulo)
+
+turtle.mainloop()           # Evita que la ventana se cierre al terminar
